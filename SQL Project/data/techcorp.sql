7a28857ae8a54cdc6641806e07abc2701fed0332
putrieltarie <putrilestari.eltarie@gmail.com>
2025-07-12 16:36:31 +0700

    1. Identify the top three customers with the highest total order amounts

--- SQL Project/data/techcorp.sql
+++ SQL Project/data/techcorp.sql
@@ -1,4 +1,12 @@
 USE myskill;
 
-
-
+-- 1. Identify the top three customers with the highest total order amounts
+select c.first_name, 
+c.last_name,
+sum(o.total_amount) total_order_amount
+from Customers as c
+join Orders o ON o.customer_id = c.customer_id
+group by c.customer_id
+order by total_order_amount desc
+limit 3
+;
