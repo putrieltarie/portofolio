0af2f8038714fbefc38f496a3b859ecfeb78f7fe
putrieltarie <putrilestari.eltarie@gmail.com>
2026-05-15 21:58:26 +0700

    membagi data ke dalam x1 consumer goods dan x2 producer goods

--- R Project/multivariat 2 sample test_t hotelling.R
+++ R Project/multivariat 2 sample test_t hotelling.R
@@ -3,3 +3,8 @@ library(readxl)
 #IMPORT DATA
 mydata <- read_excel("D:/WORK/04 REMOTE/portofolio/R Project/data/02 consumer_producer goods.xlsx",
                      sheet = "data_new format")
+
+# membagi data ke dalam x1 consumer goods dan x2 producer goods
+x = mydata
+x1 = x[which(x$jenis =="consumer goods"), ]
+x2 = x[which(x$jenis=="producer goods"), ]
